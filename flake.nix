007979732e393245f70e12972d07624a0e9b4a8e
Alexis Hildebrandt <afh@surryhill.net>
2023-03-21 11:04:26 +0100

    Add doc-ledger and doc-ledger-mode to flake.nix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,18 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    # NOTA BENE: When a new release of ledger or ledger-mode is available, update
+    # the tag in the url below and run `nix flake update`, so that GitHub Actions
+    # will build the documentation for ledger-cli.org from the latest release.
+    # TODO: Uncomment once a ledger release contains changes from afh/ledger@web-docs
+    #ledger.url = "github:ledger/ledger/v3.3.1";
+    # TODO: Remove once a ledger release contains changes from afh/ledger@web-docs
+    ledger.url = "github:afh/ledger/7b7bb08f5c5903566f866af23acde6a3c451b7d3?dir=doc";
+    ledger-mode.url = "github:ledger/ledger-mode/v4.0.0?dir=doc";
+    ledger-mode.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ledger, ledger-mode }:
     flake-utils.lib.eachDefaultSystem( system:
     let
       pkgs = import nixpkgs { inherit system; };
@@ -17,14 +26,18 @@
         inherit ruby;
         gemdir = ./.;
       };
+      tex = with pkgs; texlive.combine {
+        inherit (texlive) collection-plaingeneric collection-fontsrecommended;
+      };
     in
     {
       packages = rec {
-        default = pkgs.stdenvNoCC.mkDerivation rec {
+        default = website;
+        website = pkgs.stdenvNoCC.mkDerivation rec {
           name = "ledger.github.io";
           src = self;
 
-          phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+          dontConfigure = true;
 
           nativeBuildInputs = with pkgs; [ gems ruby nodejs ];
 
@@ -40,10 +53,52 @@
             runHook postInstall
           '';
         };
-      };
 
-      devShell = pkgs.mkShell {
-        buildInputs = self.packages.${system}.default.nativeBuildInputs;
+       doc-ledger = pkgs.stdenvNoCC.mkDerivation rec {
+         name = "ledger";
+
+         src = ledger;
+
+         nativeBuildInputs = with pkgs; [
+           cmake groff texinfo tex
+         ];
+
+         enableParallelBuilding = false;
+
+         preConfigure = "cd doc";
+
+         cmakeFlags = [ "-DBUILD_WEB_DOCS:BOOL=ON" "-Wno-dev" ];
+
+         buildFlags = "doc";
+       };
+
+       doc-ledger-mode = pkgs.stdenvNoCC.mkDerivation rec {
+         name = "ledger-mode";
+
+         src = ledger-mode;
+         nativeBuildInputs = with pkgs; [
+           texinfo tex
+         ];
+
+         enableParallelBuilding = false;
+
+         dontConfigure = true;
+
+         preBuild = "cd doc";
+         buildPhase = ''
+           runHook preBuild
+           texi2pdf --batch ledger-mode.texi
+           makeinfo --force --html --no-split ledger-mode.texi
+           runHook postBuild
+         '';
+
+         installPhase = ''
+           runHook preInstall
+           mkdir -p $out
+           cp ledger-mode.html ledger-mode.pdf $out/
+           runHook postInstall
+         '';
+       };
       };
     });
 }
